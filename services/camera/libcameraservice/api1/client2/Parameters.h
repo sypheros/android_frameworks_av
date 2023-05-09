c66cb266c7f8e940336ade9878cf1c9c83a71f8b
Ardjlon <carlosardany@gmail.com>
2023-05-09 08:53:17 +0530

    Revert "cameraserver: Correct camera1 -> api2 shim layer handling for video stabilization."
    This reverts commit 42b180d7ed0ec4599284ef49a092f14eef5b3708.

--- services/camera/libcameraservice/api1/client2/Parameters.h
+++ services/camera/libcameraservice/api1/client2/Parameters.h
@@ -147,7 +147,6 @@ struct Parameters {
 
     bool recordingHint;
     bool videoStabilization;
-    bool videoStabilizationOnSupported = false;
 
     CameraParameters2 params;
     String8 paramsFlattened;
